692374297fc422c3de9c13baa6e07707c4249f08
honnip <git@papepati.page>
2023-02-07 23:10:05 +0900

    docs: update `list` example

--- examples/list.rs
+++ examples/list.rs
@@ -1,15 +1,11 @@
 use libtos::IpfArchive;
 
 fn main() {
-    let file = std::fs::File::open("one_patch.ipf").unwrap();
+    let file = std::fs::File::open("path/to/patch.ipf").unwrap();
     let mut ipf = IpfArchive::new(file).unwrap();
 
     for i in 0..ipf.len() {
         let entry = ipf.by_index(i).unwrap();
-        println!(
-            "{}/{}",
-            entry.archive_name().display(),
-            entry.path().display()
-        );
+        println!("{}: {}", i, entry.full_path().display());
     }
 }
