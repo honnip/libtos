38799e6f5a52621104cf5b67b51720e877a6a3ef
honnip <git@papepati.page>
2023-02-08 18:14:31 +0900

    docs: write files to `extract/`

--- examples/extract.rs
+++ examples/extract.rs
@@ -3,7 +3,7 @@ use std::io;
 use libtos::{IpfArchive, IpfError};
 
 fn main() -> Result<(), IpfError> {
-    let mut ipf = IpfArchive::open("path/to/patch")?;
+    let mut ipf = IpfArchive::open("ipfes/369519_001001.ipf")?;
     for i in 0..ipf.len() {
         let mut entry = ipf.by_index(i)?;
 
@@ -12,7 +12,8 @@ fn main() -> Result<(), IpfError> {
                 std::fs::create_dir_all(p).unwrap();
             }
         }
-        let mut file = std::fs::File::create(entry.full_path())?;
+        let path = std::path::Path::new("extract").join(entry.full_path());
+        let mut file = std::fs::File::create(path)?;
         io::copy(&mut entry, &mut file)?;
     }
     Ok(())
