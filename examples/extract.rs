2a534a0a2e0339d90187719b92ebc24779caab1c
honnip <git@papepati.page>
2023-02-09 12:34:19 +0900

    docs: update extract example

--- examples/extract.rs
+++ examples/extract.rs
@@ -3,16 +3,16 @@ use std::io;
 use libtos::{IpfArchive, IpfError};
 
 fn main() -> Result<(), IpfError> {
-    let mut ipf = IpfArchive::open("ipfes/369519_001001.ipf")?;
+    let mut ipf = IpfArchive::open("path/to/patch.ipf")?;
     for i in 0..ipf.len() {
         let mut entry = ipf.by_index(i)?;
+        let path = std::path::Path::new("extract").join(entry.full_path());
 
-        if let Some(p) = entry.full_path().parent() {
+        if let Some(p) = path.parent() {
             if !p.exists() {
                 std::fs::create_dir_all(p).unwrap();
             }
         }
-        let path = std::path::Path::new("extract").join(entry.full_path());
         let mut file = std::fs::File::create(path)?;
         io::copy(&mut entry, &mut file)?;
     }
